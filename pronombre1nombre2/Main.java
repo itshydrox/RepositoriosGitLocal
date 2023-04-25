94ba80ebb573c13d37d6faf3c064b4f83db692b5
NicolasVargas211 <131809676+NicolasVargas211@users.noreply.github.com>
2023-04-25 21:36:11 +0200

    cuaerto cambio

--- pronombre1nombre2/Main.java
+++ pronombre1nombre2/Main.java
@@ -1,8 +1,10 @@
-public class Main {
+qpublic class Main {
    public static void main(String[] args) {
       System.out.println("Hola, este es mi primer proyecto en Git");
       System.out.println("Hola, este es mi segundo proyecto en Git");
       System.out.println("Hola, este es mi segundo 2 proyecto en Git");
       System.out.println("Hola, este es mi tercer proyecto en Git");
+            System.out.println("Hola, este es mi cuarto proyecto en Git");
+
    }
 }
