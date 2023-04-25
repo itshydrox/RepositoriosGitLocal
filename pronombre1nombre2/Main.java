082bf4b6553d1a908acbbc64ca5f5a364c4845dd
MouadHydrox <idrissimou3ad@gmail.com>
2023-04-25 20:32:16 +0200

    Segundo cambio en Main.java

--- pronombre1nombre2/Main.java
+++ pronombre1nombre2/Main.java
@@ -2,5 +2,6 @@ public class Main {
    public static void main(String[] args) {
       System.out.println("Hola, este es mi primer proyecto en Git");
       System.out.println("Hola, este es mi segundo proyecto en Git");
+      System.out.println("Hola, este es mi segundo 2 proyecto en Git");
    }
 }
