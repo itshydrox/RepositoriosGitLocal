e598fd4ebfbc09d9155f5188067afe6c9bcc03df
MouadHydrox <idrissimou3ad@gmail.com>
2023-04-25 22:04:33 +0200

    cambio

--- pronombre1nombre2/Main.java
+++ pronombre1nombre2/Main.java
@@ -1,4 +1,4 @@
-qpublic class Main {
+public class Main {
    public static void main(String[] args) {
       System.out.println("Hola, este es mi primer proyecto en Git");
       System.out.println("Hola, este es mi segundo proyecto en Git");
